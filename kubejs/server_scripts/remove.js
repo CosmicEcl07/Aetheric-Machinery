a716c768efaea11442bde1899156ea9f4f9c5c6d
CosmicEcl07 <joeyachristopher@gmail.com>
2025-12-07 15:20:38 -0600

    Mod Additions

--- kubejs/server_scripts/remove.js
+++ kubejs/server_scripts/remove.js
@@ -1,1 +1,8 @@
-event.remove({mod: 'exdeorum'})
+ServerEvents.recipes(event => {
+    event.remove({
+        mod: 'exdeorum',
+        not: { output: '#exdeorum:hammers' },
+        not: { output: '#exdeorum:crooks' }
+
+    })
+})
