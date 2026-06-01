49ebfea9d3d50a6da802e50eb261031f73cee8ef
CosmicEcl07 <joeyachristopher@gmail.com>
2026-06-01 13:50:22 -0500

    - Made ULV Input Hatch Craftable - Mod Updates - Reformated QB - Added Tips & Trick Page - Made Effortless Building Upgrades Craftable

--- kubejs/server_scripts/remove.js
+++ kubejs/server_scripts/remove.js
@@ -25,5 +25,9 @@ ServerEvents.recipes(event => {
     // ModularRouters Removals
     event.remove('modularrouters:blank_module');
 
-
+    // Effortless Building Removals
+    event.remove('effortlessbuilding:reach_upgrade1');
+    event.remove('effortlessbuilding:reach_upgrade2');
+    event.remove('effortlessbuilding:reach_upgrade3');
+    
 });
